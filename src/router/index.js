d553acaa9f8705ac0b10f64812fe900429d160d5
JhoanDev <jhoanm499@gmail.com>
2026-03-30 11:19:54 -0500

    d2

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const routes = [
@@ -15,7 +15,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory('/jhoanMoreno-s-website/'),
+  history: createWebHashHistory(),
   routes
 })
 
